bb2a66c5716281ba74dfb4c7855e44cd039e859b
Saubhagya3 <saubhagyathapa3@gmail.com>
2020-05-31 05:00:46 +1000

    w

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import MobileHeader from './mobileHeader'
 import DesktopHeader from './desktopHeader'
 import useWindowSize from '@react-hook/window-size/throttled'
 
 const Header = () => {
-    const [ winWidth, setWinWidth] = useState(window.innerWidth);
-    const breakpoint = 600;
+    const [ width ] = useWindowSize()
 
-    useEffect(() => {
-        const handleWindowResize = () => setWinWidth(window.innerWidth)
-        window.addEventListener("resize", handleWindowResize);
-        // Return a function from the effect that removes the event listener
-        return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
-
-    return winWidth > breakpoint ? 
+    return width > 550 ? 
         <DesktopHeader />
         :
         <MobileHeader />

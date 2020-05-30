4aecd02c098efa79aa1ccb5c783c85db299ca9c3
Saubhagya3 <saubhagyathapa3@gmail.com>
2020-05-31 05:25:28 +1000

    x

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,8 @@ import useWindowSize from '@react-hook/window-size/throttled'
 
 const Header = () => {
     const [ width ] = useWindowSize()
-
-    return width > 550 ? 
-        <DesktopHeader />
-        :
-        <MobileHeader />
+    
+    return width > 550 ? <DesktopHeader /> : <MobileHeader />
 }        
 
 export default Header
--- src/components/mobileHeader.js
+++ src/components/mobileHeader.js
@@ -5,42 +5,42 @@ import menuIcon1 from '../images/menu-icon.png'
 import menuIcon2 from '../images/close-menu-icon.png'
 
 const MobileHeader = () => {
-    const [ navList, setNavList ] = useState(mobileHeaderStyles.navList)
+    const [ mnavList, setMNavList ] = useState(mobileHeaderStyles.mnavList)
     const [ sideText, setSideText ] = useState(menuIcon1)
-    const  [ clicked, setClicked ] = useState(false)
+    const  [ mclicked, setMClicked ] = useState(false)
     const handleSideBarClick = () => {
-        setClicked(!clicked)
-        clicked ? setNavList(mobileHeaderStyles.navList) : setNavList(mobileHeaderStyles.navList2)
-        clicked ? setSideText(menuIcon1) : setSideText(menuIcon2)
+        setMClicked(!mclicked)
+        mclicked ? setMNavList(mobileHeaderStyles.mnavList) : setMNavList(mobileHeaderStyles.mnavList2)
+        mclicked ? setSideText(menuIcon1) : setSideText(menuIcon2)
     }
 
     return(
         <header>
-            <div className={mobileHeaderStyles.main}>
-                <div className={mobileHeaderStyles.sidebarButton} onClick={handleSideBarClick}>
+            <div className={mobileHeaderStyles.mmain}>
+                <div className={mobileHeaderStyles.msidebarButton} onClick={handleSideBarClick}>
                     <img src={sideText} style={{height: "2rem", width: "2.4rem"}}/>
                 </div>
-                <div className={mobileHeaderStyles.title}><h1><Link to='/'>TronEcon.</Link></h1></div>
+                <div className={mobileHeaderStyles.mtitle}><h1><Link to='/'>TronEcon.</Link></h1></div>
             </div>
             <nav>
-                <ul className={navList}>
+                <ul className={mnavList}>
                     <li>
-                        <h3><Link to="/" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>Home</Link></h3>
+                        <h3><Link to="/" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.mactiveNavItem}>Home</Link></h3>
                     </li>
                     <li>
-                        <h3><Link to="/economy" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>Economy</Link></h3>
+                        <h3><Link to="/economy" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.activeMNavItem}>Economy</Link></h3>
                     </li>
                     <li>
-                        <h3><Link to="/equity" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>Equity</Link></h3>
+                        <h3><Link to="/equity" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.activeMNavItem}>Equity</Link></h3>
                     </li>
                     <li>
-                        <h3><Link to="/tech" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>Tech</Link></h3>
+                        <h3><Link to="/tech" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.activeMNavItem}>Tech</Link></h3>
                     </li>
                     <li>
-                        <h3><Link to="/about" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>About</Link></h3>
+                        <h3><Link to="/about" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.activeMNavItem}>About</Link></h3>
                     </li>
                     <li>
-                        <h3><Link to="/contact" className={mobileHeaderStyles.navItem} activeClassName={mobileHeaderStyles.activeNavItem}>Contact</Link></h3>
+                        <h3><Link to="/contact" className={mobileHeaderStyles.mnavItem} activeClassName={mobileHeaderStyles.activeMNavItem}>Contact</Link></h3>
                     </li>
                 </ul>
             </nav>

76d0eb043dc19d337ac3e9c2d66b5a3437b0f581
Saubhagya3 <saubhagyathapa3@gmail.com>
2020-05-31 04:19:06 +1000

    re re re

--- src/components/desktopHeader.js
+++ src/components/desktopHeader.js
@@ -1,4 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import headerStyles from './desktopHeader.module.scss'
 
-const DesktopHeader
+const DesktopHeader = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title,
+                    author
+                }
+            }
+        }
+    `) 
+    return (
+        <header className={headerStyles.header}>
+            <div className={headerStyles.ticker}>
+                <div className={headerStyles.tickerItem}>
+                    <p>Item One</p>
+                </div>
+                <div className={headerStyles.tickerItem}>
+                    <p>Item Two</p>
+                </div>
+                <div className={headerStyles.tickerItem}>
+                    <p>Item Three</p>
+                </div>
+                <div className={headerStyles.tickerItem}>
+                    <p>Item Four</p>
+                </div>
+            </div>
+            <div className={headerStyles.banner}>
+                <h1 style={{color: '#f49530', marginBottom: '0'}}>
+                    <Link className={headerStyles.title} to="/">
+                        {data.site.siteMetadata.title}
+                    </Link>. 
+                </h1>
+                
+            </div>
+            <div className={headerStyles.mainnav}> 
+                <div className={headerStyles.nav}>
+                <ul className={headerStyles.navList}>
+                    <li>
+                        <h3><Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Home</Link></h3>
+                    </li>
+                    <li>
+                        <h3><Link to="/economy" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Economy</Link></h3>
+                    </li>
+                    <li>
+                        <h3><Link to="/equity" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Equity</Link></h3>
+                    </li>
+                    <li>
+                        <h3><Link to="/tech" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Tech</Link></h3>
+                    </li>
+                </ul>
+                </div>
+                {/* <div className={headerStyles.login}>
+                    <p style={{color:'silver'}}><Link to="/404">Sign In</Link> | <Link to="/404">Subscribe</Link></p>
+                </div> */}
+            </div>
+        </header>
+    )
+}
+
+export default DesktopHeader
--- src/components/header.js
+++ src/components/header.js
@@ -1,78 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
-import headerStyles from './header.module.scss'
 import MobileHeader from './mobileHeader'
+import DesktopHeader from './desktopHeader'
 import useWindowSize from '@react-hook/window-size/throttled'
 
 const Header = () => {
-    const [ width, height ] = useWindowSize()
-    // const [ winWidth, setWinWidth] = useState(width);
+    const [ width ] = useWindowSize()
+    const [ winWidth, setWinWidth] = useState(width);
     const breakpoint = 550;
 
-    // useEffect(() => {
-    //     const handleWindowResize = () => setWinWidth(window.innerWidth)
-    //     window.addEventListener("resize", handleWindowResize);
+    useEffect(() => {
+        const handleWindowResize = () => setWinWidth(window.innerWidth)
+        window.addEventListener("resize", handleWindowResize);
 
-    //     // Return a function from the effect that removes the event listener
-    //     return () => window.removeEventListener("resize", handleWindowResize);
-    // }, []);
+        // Return a function from the effect that removes the event listener
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
 
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    title,
-                    author
-                }
-            }
-        }
-    `) 
-    return width > breakpoint ? 
-        <header className={headerStyles.header}>
-            <div className={headerStyles.ticker}>
-                <div className={headerStyles.tickerItem}>
-                    <p>Item One</p>
-                </div>
-                <div className={headerStyles.tickerItem}>
-                    <p>Item Two</p>
-                </div>
-                <div className={headerStyles.tickerItem}>
-                    <p>Item Three</p>
-                </div>
-                <div className={headerStyles.tickerItem}>
-                    <p>Item Four</p>
-                </div>
-            </div>
-            <div className={headerStyles.banner}>
-                <h1 style={{color: '#f49530', marginBottom: '0'}}>
-                    <Link className={headerStyles.title} to="/">
-                        {data.site.siteMetadata.title}
-                    </Link>. 
-                </h1>
-                
-            </div>
-            <div className={headerStyles.mainnav}> 
-                <div className={headerStyles.nav}>
-                <ul className={headerStyles.navList}>
-                    <li>
-                        <h3><Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Home</Link></h3>
-                    </li>
-                    <li>
-                        <h3><Link to="/economy" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Economy</Link></h3>
-                    </li>
-                    <li>
-                        <h3><Link to="/equity" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Equity</Link></h3>
-                    </li>
-                    <li>
-                        <h3><Link to="/tech" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Tech</Link></h3>
-                    </li>
-                </ul>
-                </div>
-                {/* <div className={headerStyles.login}>
-                    <p style={{color:'silver'}}><Link to="/404">Sign In</Link> | <Link to="/404">Subscribe</Link></p>
-                </div> */}
-            </div>
-        </header>
+    return winWidth > breakpoint ? 
+        <DesktopHeader />
         :
         <MobileHeader />
 }        
